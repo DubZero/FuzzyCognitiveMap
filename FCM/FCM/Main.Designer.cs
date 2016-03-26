05064f2af29024115fd8564e8f1bf5672dadfe34
DubZero <shumkinalexandrne@gmail.com>
2016-03-26 20:57:12 +0300

    Добавление гетеров и сетеров создание конструктора

--- FCM/FCM/Main.Designer.cs
+++ FCM/FCM/Main.Designer.cs
@@ -57,7 +57,7 @@
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(270, 24);
             this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Text = "menuStrip";
             // 
             // открытьToolStripMenuItem
             // 
