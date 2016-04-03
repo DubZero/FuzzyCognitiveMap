b0016be9ccaaf14d72c7c1eaae0cd84f7425a191
DubZero <shumkinalexandrne@gmail.com>
2016-04-03 15:23:39 +0300

    Создание класса Settings

---
+++ FCM/FCM/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCM
+{
+    public class Settings
+    {
+        // Вид функции
+        public int Function { get; set; }
+        // Вид аргумента функции
+        public int ArtFunc { get; set; }
+        // Сохранение в XLS
+        public bool SaveToXLS { get; set; }
+        // Расширенный отчет
+        public bool AndvancedReport { get; set; }        
+    }
+}
