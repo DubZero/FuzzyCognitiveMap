ecd3c83d5e74e5e4db11990729f9b13e5ba76f09
DubZero <shumkinalexandrne@gmail.com>
2016-04-20 17:24:30 +0300

    Не добавилось при первом коммите

--- FCM/FCM/DrawGraph.cs
+++ FCM/FCM/DrawGraph.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FCM
 {
+    class GraphVertex
+    {
+        public int x, y;
+
+        public GraphVertex(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Edge
+    {
+        public int v1, v2;
+
+        public Edge(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
     class DrawGraph
     {
+        Bitmap bitmap;
+
+        public Bitmap GetBitmap()
+        {
+            return bitmap;
+        }
     }
 }
--- FCM/FCM/Graph.Designer.cs
+++ FCM/FCM/Graph.Designer.cs
@@ -28,11 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(737, 425);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(314, 444);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 1;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            // 
+            // Graph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 472);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Graph";
             this.Text = "Graph";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonBack;
     }
 }
