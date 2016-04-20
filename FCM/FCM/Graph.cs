7bb0534c2e9e3fb9c915295a2cc73159ab008b06
ВладТрубицин <trubitsins@gmail.com>
2016-04-20 22:22:53 +0300

    Плешь

--- FCM/FCM/Graph.cs
+++ FCM/FCM/Graph.cs
@@ -28,7 +28,7 @@ namespace FCM
             double VertDiag;
             VertDiag= Math.Sqrt(VertRad * VertRad + VertRad * VertRad);
             double x1, y1,x2,y2,y,x,k=0;
-            while (k <= VertDiag*2)
+            while (k < VertDiag*2)
             {
                 rad *= 1.2;
                 x1 = rad * Math.Cos(360 / ArrVertex.Count()) + pictureBox.Height / 2;
@@ -46,7 +46,7 @@ namespace FCM
         {
             for(int i=0;i<ArrVertex.Count();i++)
             {
-                GraphV[i]=new GraphVertex((rad * Math.Cos(i*360 / ArrVertex.Count()) + pictureBox.Height / 2)-VertRad,(rad * Math.Cos(i*360 / ArrVertex.Count()) + pictureBox.Width / 2)-VertRad);
+                GraphV[i]=new GraphVertex((rad * Math.Sin(i*360 / ArrVertex.Count()) + pictureBox.Width / 2)-VertRad,(rad * Math.Cos(i*360 / ArrVertex.Count()) + pictureBox.Height / 2)-VertRad);
             }
         }
 
@@ -55,9 +55,8 @@ namespace FCM
             GraphV = new GraphVertex[ArrVertex.Count()];
             double radius=calcRad();
             Pen pen = new Pen(Color.Black);
-            pen.Width = 5;
+            pen.Width = 2;
             Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
-            //pictureBox.Image = bitmap;
             Graphics g = Graphics.FromImage(bitmap);
             calcVert(radius);
             for (int i=0;i<GraphV.Count();i++)
