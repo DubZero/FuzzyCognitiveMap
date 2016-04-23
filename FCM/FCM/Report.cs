516d37e181de68578cf97ed05110173d07386d48
DubZero <shumkinalexandrne@gmail.com>
2016-04-23 11:54:53 +0300

    Рабочий график + сохранение в файл

--- FCM/FCM/Report.cs
+++ FCM/FCM/Report.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ZedGraph;
 using System.Threading;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -44,75 +43,38 @@ namespace FCM
                     reportTable[i + 1, j].Value = Math.Round(Vertexes[i].Values[j],3);
                 }
             }
-            DrawGraph();
-            DrawChartGraph();
             
-        }
-        // Отрисовка графика
-        public void DrawGraph()
-        {
-            GraphPane pane = zedGraph.GraphPane;
-            pane.Title = "Analysis Chart";
-            pane.CurveList.Clear();
-
-            pane.XAxis.Title = "Time";
-            pane.YAxis.Title = "Nodes value";
-            PointPairList list = new PointPairList();
-            for (int j = 1; j < reportTable.ColumnCount; j++)
-            {
-                for (int i = 0; i < reportTable.RowCount; i++)
-                {
-
-                    double x = Convert.ToDouble(reportTable[0, i].Value);
-                    double y = Convert.ToDouble(reportTable[j, i].Value);
-                    list.Add(x, y);
-                }
-                // Слип для генерации новых чисел P.S.(сид генерации берется из времени)
-                Random randomGen = new Random();
-                int RedComponent = randomGen.Next(255);
-                Thread.Sleep(4);
-                int GreenComponent = randomGen.Next(255);
-                Thread.Sleep(4);
-                int BlueComponent = randomGen.Next(255);
-                Thread.Sleep(4);
-                Color randomColor = Color.FromArgb(RedComponent, GreenComponent, BlueComponent);
-                LineItem myCurve = pane.AddCurve(Vertexes[j-1].Name, list, randomColor, SymbolType.None);
-                myCurve.Line.Width = 2.0f;
-                list.Clear();
-                
-            }
-            
-            zedGraph.AxisChange();
-            zedGraph.Invalidate();
-        }
+            DrawChartGraph();            
+        }       
 
         void DrawChartGraph()
         {
             List<Series> lstSeries = new List<Series>();
-            for (int j = 1; j < reportTable.ColumnCount; j++)
+            for (int j = 0; j < reportTable.ColumnCount-1; j++)
             {
-                lstSeries.Add(new Series());
+                lstSeries.Add(new Series(Vertexes[j].Name));
+                lstSeries[j].ChartType = SeriesChartType.Line;
+                
                 for (int i = 0; i < reportTable.RowCount; i++)
                 {
 
                     double x = Convert.ToDouble(reportTable[0, i].Value);
-                    double y = Convert.ToDouble(reportTable[j, i].Value);
-                    lstSeries[j - 1].Points.AddXY(x, y);
+                    double y = Convert.ToDouble(reportTable[j+1, i].Value);
+                    lstSeries[j].Points.AddXY(x, y);
                 }            
             }
-           
-            for (int j = 0; j < lstSeries.Count();j++)
+            foreach(Series series in lstSeries)
             {
-                lstSeries[j].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-                chart.Series.Add(Vertexes[j].Name);
-                for (int i = 0; i < lstSeries[0].Points.Count(); i++)
-                {
-                   // chart.Series[Vertexes[j].Name].Points.AddXY(lstSeries[j].Points[i].XValue, lstSeries[j].Points[i].YValues);
-                }
-            }
+                chart.Series.Add(series);                
+            }            
+            for(int i = 0; i < lstSeries.Count(); i++)
+            {
+                chart.Series[i].BorderWidth = 4;
+            }            
+
         }
         // Сохранение графика в файл
-        public void SaveButton_Click(object sender, EventArgs e)
+        /*public void SaveButton_Click(object sender, EventArgs e)
         {
             // ДИалог выбора имени файла создаем вручную
             SaveFileDialog dlg = new SaveFileDialog();
@@ -145,7 +107,7 @@ namespace FCM
                     bmp.Save(dlg.FileName);
                 }
             }
-        }
+        }*/
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -154,6 +116,32 @@ namespace FCM
                 gr.ArrVertex = Vertexes;
                 gr.Matr = Matr;
                 gr.ShowDialog();
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // ДИалог выбора имени файла создаем вручную
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "*.png|*.png|*.jpg; *.jpeg|*.jpg;*.jpeg|*.bmp|*.bmp|Все файлы|*.*";
+
+            
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {                          
+                // Формат картинки выбирается исходя из имени выбранного файла
+                if (dlg.FileName.EndsWith(".png"))
+                {
+                    chart.SaveImage(dlg.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                }
+                else if (dlg.FileName.EndsWith(".jpg") || dlg.FileName.EndsWith(".jpeg"))
+                {
+                    chart.SaveImage(dlg.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                else if (dlg.FileName.EndsWith(".bmp"))
+                {
+                    chart.SaveImage(dlg.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
             }
         }
     }
