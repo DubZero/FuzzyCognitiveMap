bb68723584f216949f110726558560d87e472055
DubZero <shumkinalexandrne@gmail.com>
2016-05-08 14:23:04 +0300

    Функция сохранения в dat файл

--- FCM/FCM/Report.Designer.cs
+++ FCM/FCM/Report.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.button1 = new System.Windows.Forms.Button();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.chbOutput = new System.Windows.Forms.CheckBox();
@@ -42,6 +42,7 @@
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.buttonSave = new System.Windows.Forms.Button();
+            this.toDatFile = new System.Windows.Forms.Button();
             this.tabPage3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.tabPage1.SuspendLayout();
@@ -108,31 +109,31 @@
             this.chart.BorderlineWidth = 4;
             this.chart.BorderSkin.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
             this.chart.BorderSkin.PageColor = System.Drawing.Color.Transparent;
-            chartArea1.AxisX.Crossing = 0D;
-            chartArea1.AxisX.IsMarginVisible = false;
-            chartArea1.AxisX.Minimum = 0D;
-            chartArea1.AxisX.Title = "Time";
-            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            chartArea1.AxisY.Crossing = 0D;
-            chartArea1.AxisY.Maximum = 1D;
-            chartArea1.AxisY.Minimum = 0D;
-            chartArea1.AxisY.Title = "Nodes values";
-            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            chartArea1.BackColor = System.Drawing.Color.White;
-            chartArea1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
-            chartArea1.BackSecondaryColor = System.Drawing.Color.WhiteSmoke;
-            chartArea1.BorderColor = System.Drawing.Color.Transparent;
-            chartArea1.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea1);
-            legend1.BackColor = System.Drawing.Color.Gray;
-            legend1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
-            legend1.BackSecondaryColor = System.Drawing.Color.DarkGray;
-            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
-            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            legend1.IsTextAutoFit = false;
-            legend1.Name = "Legend1";
-            legend1.TitleBackColor = System.Drawing.Color.Transparent;
-            this.chart.Legends.Add(legend1);
+            chartArea3.AxisX.Crossing = 0D;
+            chartArea3.AxisX.IsMarginVisible = false;
+            chartArea3.AxisX.Minimum = 0D;
+            chartArea3.AxisX.Title = "Time";
+            chartArea3.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea3.AxisY.Crossing = 0D;
+            chartArea3.AxisY.Maximum = 1D;
+            chartArea3.AxisY.Minimum = 0D;
+            chartArea3.AxisY.Title = "Nodes values";
+            chartArea3.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            chartArea3.BackColor = System.Drawing.Color.White;
+            chartArea3.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
+            chartArea3.BackSecondaryColor = System.Drawing.Color.WhiteSmoke;
+            chartArea3.BorderColor = System.Drawing.Color.Transparent;
+            chartArea3.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea3);
+            legend3.BackColor = System.Drawing.Color.Gray;
+            legend3.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            legend3.BackSecondaryColor = System.Drawing.Color.DarkGray;
+            legend3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            legend3.IsTextAutoFit = false;
+            legend3.Name = "Legend1";
+            legend3.TitleBackColor = System.Drawing.Color.Transparent;
+            this.chart.Legends.Add(legend3);
             this.chart.Location = new System.Drawing.Point(-4, 3);
             this.chart.Name = "chart";
             this.chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
@@ -151,25 +152,25 @@
         System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0))))),
         System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0))))),
         System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))))};
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            series1.IsValueShownAsLabel = true;
-            series1.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
-            series1.LabelBorderWidth = 2;
-            series1.Legend = "Legend1";
-            series1.MarkerBorderColor = System.Drawing.Color.White;
-            series1.MarkerBorderWidth = 4;
-            series1.MarkerColor = System.Drawing.Color.White;
-            series1.Name = "Series1";
-            this.chart.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series3.IsValueShownAsLabel = true;
+            series3.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series3.LabelBorderWidth = 2;
+            series3.Legend = "Legend1";
+            series3.MarkerBorderColor = System.Drawing.Color.White;
+            series3.MarkerBorderWidth = 4;
+            series3.MarkerColor = System.Drawing.Color.White;
+            series3.Name = "Series1";
+            this.chart.Series.Add(series3);
             this.chart.Size = new System.Drawing.Size(1044, 598);
             this.chart.TabIndex = 0;
             this.chart.Text = "chart1";
-            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            title1.Name = "Title1";
-            title1.Text = "Analysis Chart";
-            this.chart.Titles.Add(title1);
+            title3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title3.Name = "Title1";
+            title3.Text = "Analysis Chart";
+            this.chart.Titles.Add(title3);
             // 
             // tabPage1
             // 
@@ -229,12 +230,23 @@
             this.buttonSave.UseVisualStyleBackColor = true;
             this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
+            // toDatFile
+            // 
+            this.toDatFile.Location = new System.Drawing.Point(411, 650);
+            this.toDatFile.Name = "toDatFile";
+            this.toDatFile.Size = new System.Drawing.Size(163, 23);
+            this.toDatFile.TabIndex = 2;
+            this.toDatFile.Text = "Экспорт в .dat файл";
+            this.toDatFile.UseVisualStyleBackColor = true;
+            this.toDatFile.Click += new System.EventHandler(this.toDatFile_Click);
+            // 
             // Report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(1058, 675);
+            this.Controls.Add(this.toDatFile);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tabControl1);
@@ -263,5 +275,6 @@
         private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.CheckBox chbOutput;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button toDatFile;
     }
 }
--- FCM/FCM/Report.cs
+++ FCM/FCM/Report.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.IO;
 
 
 namespace FCM
@@ -32,54 +33,56 @@ namespace FCM
             for (int i = 0; i < Vertexes.Count(); i++)
             {
                 reportTable.Columns.Add(Vertexes[i].Name, Vertexes[i].Name);
-                
+
             }
-            for(int j = 0; j < Vertexes[0].Values.Count(); j++)
-            if(Vertexes[0].Values.Count() > reportTable.RowCount)
-                reportTable.Rows.Add();
+            for (int j = 0; j < Vertexes[0].Values.Count(); j++)
+                if (Vertexes[0].Values.Count() > reportTable.RowCount)
+                    reportTable.Rows.Add();
+
             //вывод значений концептов
-            for(int i=0;i<Vertexes.Count();i++)
+            for (int i = 0; i < Vertexes.Count(); i++)
             {
                 for (int j = 0; j < Vertexes[0].Values.Count(); j++)
                 {
                     reportTable[0, j].Value = j;
-                    reportTable[i + 1, j].Value = Math.Round(Vertexes[i].Values[j],3);
+                    reportTable[i + 1, j].Value = Math.Round(Vertexes[i].Values[j], 3);
                 }
             }
-            
-            DrawChartGraph();            
-        }       
+
+
+            DrawChartGraph();
+        }
         //рисование графика
         void DrawChartGraph()
         {
             List<Series> lstSeries = new List<Series>();
             //добавление элементов
-            for (int j = 0; j < reportTable.ColumnCount-1; j++)
+            for (int j = 0; j < reportTable.ColumnCount - 1; j++)
             {
                 lstSeries.Add(new Series(Vertexes[j].Name));
                 lstSeries[j].ChartType = SeriesChartType.Line;
-                
+
 
                 for (int i = 0; i < reportTable.RowCount; i++)
                 {
                     //задание точек 
                     double x = Convert.ToDouble(reportTable[0, i].Value);
-                    double y = Convert.ToDouble(reportTable[j+1, i].Value);
+                    double y = Convert.ToDouble(reportTable[j + 1, i].Value);
                     lstSeries[j].Points.AddXY(x, y);
-                }            
+                }
             }
-            foreach(Series series in lstSeries)
+            foreach (Series series in lstSeries)
             {
                 //добавление графика
-                chart.Series.Add(series);                
-            }            
-            for(int i = 0; i < lstSeries.Count(); i++)
+                chart.Series.Add(series);
+            }
+            for (int i = 0; i < lstSeries.Count(); i++)
             {
-                chart.Series[i].BorderWidth = 3;                
+                chart.Series[i].BorderWidth = 3;
 
             }
 
-        }       
+        }
         //открытие окна графа
         private void button1_Click(object sender, EventArgs e)
         {
@@ -93,14 +96,14 @@ namespace FCM
         //сохранение графика в файл
         private void buttonSave_Click(object sender, EventArgs e)
         {
-            // ДИалог выбора имени файла создаем вручную
+            // Диалог выбора имени файла создаем вручную
             SaveFileDialog dlg = new SaveFileDialog();
             dlg.Filter = "*.png|*.png|*.jpg; *.jpeg|*.jpg;*.jpeg|*.bmp|*.bmp|Все файлы|*.*";
 
-            
+
 
             if (dlg.ShowDialog() == DialogResult.OK)
-            {                          
+            {
                 // Формат картинки выбирается исходя из имени выбранного файла
                 if (dlg.FileName.EndsWith(".png"))
                 {
@@ -116,27 +119,53 @@ namespace FCM
                 }
             }
         }
+
         // Убрать\Показать линии НЕ выходных значений
         private void chbOutput_CheckedChanged(object sender, EventArgs e)
         {
-            for(int i = 0; i < Vertexes.Count(); i++)
-            {                
+            for (int i = 0; i < Vertexes.Count(); i++)
+            {
                 if (!isOutputFlag)
                 {
                     if (!Vertexes[i].isOutput)
                     {
                         chart.Series[i].BorderWidth = 0;
-                    }                                     
+                    }
                 }
                 else
                 {
-                    if(!Vertexes[i].isOutput)
+                    if (!Vertexes[i].isOutput)
                     {
                         chart.Series[i].BorderWidth = 3;
-                    }                    
+                    }
                 }
             }
             isOutputFlag = chbOutput.Checked;
+        }
+        void DatFileSave(Vertex[] ArrVertex)
+        {
+            SaveFileDialog dat = new SaveFileDialog();
+            dat.Filter = "*.dat|*.dat";
+            dat.RestoreDirectory = true;
+            if (dat.ShowDialog(this) == DialogResult.OK)
+            {
+                FileStream f = new FileStream(dat.FileName, FileMode.Create, FileAccess.Write);
+                StreamWriter stm = new StreamWriter(f, System.Text.Encoding.GetEncoding(1251));
+                for (int j = 0; j < ArrVertex[0].Values.Count(); j++)
+                {
+                    //запись в файл    
+                    for (int i = 0; i < ArrVertex.Count(); i++)
+                    {
+                        stm.Write(Math.Round(ArrVertex[i].Values[j], 3) + " ");
+                    }                    
+                }
+                stm.Close();
+            }
+        }
+        // Создание dat. файла (текстовый набор данных) для R-studio
+        private void toDatFile_Click(object sender, EventArgs e)
+        {
+            DatFileSave(Vertexes);
         }
     }
 }
